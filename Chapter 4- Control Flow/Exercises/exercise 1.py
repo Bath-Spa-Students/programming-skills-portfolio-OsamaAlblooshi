f04815dd0c527d266360f0decebedc44a133435b
OSAMA ALBLOOSHI IBRAHIM <144319866+OsamaAlblooshi@users.noreply.github.com>
2023-11-06 22:30:42 +0400

    chap 4

---
+++ Chapter 4- Control Flow/Exercises/exercise 1.py
@@ -0,0 +1,9 @@
+#•Write an if statement to test whether the alien’s color is green. 
+#If it is, print a message that the player just earned 5 points.
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    pass
